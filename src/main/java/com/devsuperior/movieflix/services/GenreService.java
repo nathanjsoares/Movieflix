386c37897974c9d789acb6809cef81aca98e1e0a
Nathan Soares <nathansoares@email.com>
2026-03-25 18:08:54 -0300

    Genre implementation

--- src/main/java/com/devsuperior/movieflix/services/GenreService.java
+++ src/main/java/com/devsuperior/movieflix/services/GenreService.java
@@ -1,12 +1,21 @@
 package com.devsuperior.movieflix.services;
 
+import com.devsuperior.movieflix.dto.GenreDTO;
+import com.devsuperior.movieflix.entities.Genre;
 import com.devsuperior.movieflix.repositories.GenreRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 public class GenreService {
 
     @Autowired
     private GenreRepository repository;
+
+    public List<GenreDTO> findAll() {
+        List<Genre> result = repository.findAll();
+        return result.stream().map(GenreDTO::new).toList();
+    }
 }
